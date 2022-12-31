4286979262c731508880c77c585777c83e033011
Alessandro Proglio <ale.proglio@gmail.com>
2022-12-31 09:47:29 +0100

    updated example

--- example.py
+++ example.py
@@ -1,9 +1,16 @@
-from time import sleep
-from sngcja5 import SNGCJA5
+from machine import Pin, I2C
+from pico-sngcja5 import SNGCJA5
 
 
-pm_sensor = SNGCJA5(i2c_bus_no=3)
+# If SDA and SCL are connected to I2C bus 0, then:
+i2c = I2C(0, sda=Pin(0), scl=Pin(1))
+pm_sensor = SNGCJA5(i2c)
 
-while True:
-    print(pm_sensor.get_measurement())
-    sleep(5)
+# get oldest measurement from queue
+single_measure = pm_sensor.get_measurement()
+
+# empty_measurements_queue
+pm_sensor.empty_measurements_queue()
+
+# acquire 3 measurements
+print(pm_sensor.measurements_serie(3))
